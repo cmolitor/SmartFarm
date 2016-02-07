2f5f5a3579df37cbc12d3e11944dd09ff153cc21
christoph <christoph.molitor@posteo.de>
2016-02-07 15:31:15 +0100

    Bug fixing and clean up

--- modbus.py
+++ modbus.py
@@ -13,6 +13,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 def readChannels():
     # voltage phase 1 to neutral
+    print "New measurement readings: ", int(time.time())
     listValues = list()
     for c in listChannels:
     	handle = client.read_holding_registers(c['register'],c['words'],unit=c['unit'])
@@ -20,22 +21,22 @@ def readChannels():
     	#print c['description'],":", handle.registers[0]/float(c['factor'])
 
     for i, channel in enumerate(listChannels):
-    	print channel['description'],":", channel['uuid'], int(time.time()*1000), listValues[i]
+    	# print channel['description'],":", channel['uuid'], int(time.time()*1000), listValues[i]
     	# Here fire values into VZ middleware
     	addValue(channel['uuid'], int(time.time()*1000), listValues[i])
 
 # Add measurement value
 def addValue(uuid, timestamp, value):
 	url = strURL + "/data/" + uuid + ".json?operation=add&ts=" + str(timestamp) + "&value=" + str(value)
-	print url
+	# print url
 	req = urllib2.Request(url)
 	response = urllib2.urlopen(req)
 	jsonVZ = response.read()
-	print jsonVZ
+	# print jsonVZ
 	return 1
 
 # Create group in VZ
-def createGroup(title="Molitor", public=1):
+def createGroup(title, public=1):
 	url = strURL + "/group.json?operation=add&title=" + title + "&public=" + str(public)
 	req = urllib2.Request(url)
 	response = urllib2.urlopen(req)
@@ -69,8 +70,10 @@ def getChildren(uuid):
 	data = json.loads(getGroup(uuid))
 
 	listChildren = list()
-	for x in range(0,len(data['entity']['children'])):
-		listChildren.append(data['entity']['children'][x]['uuid'])
+	if 'entity' in data:
+		if 'children' in data['entity']:
+			for x in range(0,len(data['entity']['children'])):
+				listChildren.append(data['entity']['children'][x]['uuid'])
 	
 	return listChildren
 
@@ -90,12 +93,30 @@ def createChannel(type, title):
 	_uuid = data["entity"]["uuid"]
 	return _uuid
 
+# Test VZ installation
+def testVZ():
+	req = urllib2.Request(strURL)
+	print req
+	try:
+		response = urllib2.urlopen(req)
+	except urllib2.HTTPError as e:
+		return False
+
+	jsonVZ = response.read()
+
+	try:
+		data = json.loads(jsonVZ)
+	except ValueError, e:
+		return False
+
+	return data['version']
+
 # =======================
 # Definitions
 # =======================
 
-# Reading frequency in seconds
-frequency = 5
+print("Used Python version: ")
+print(sys.version)
 
 # Add channels
 listChannels = list()
@@ -104,46 +125,61 @@ listChannels.append({'description': "V1_ph2n", 'register': 51284, 'words': 1, 'u
 #listChannels.append({'description': "V3_ph2n", 'register': 51286, 'words': 1, 'unit': 0xFF, 'measurement': "voltage", 'factor': 100})
 listChannels.append({'description': "frequency", 'register': 51287, 'words': 1, 'unit': 0xFF, 'measurement': "frequency", 'factor': 100})
 listChannels.append({'description': "P", 'register': 51292, 'words': 1, 'unit': 0xFF, 'measurement': "activepower", 'factor': 100})
-listChannels.append({'description': "P1", 'register': 51296, 'words': 1, 'unit': 0xFF, 'measurement': "activepower", 'factor': 100})
+#listChannels.append({'description': "P1", 'register': 51296, 'words': 1, 'unit': 0xFF, 'measurement': "activepower", 'factor': 100})
 #listChannels.append({'description': "P2", 'register': 51297, 'words': 1, 'unit': 0xFF, 'measurement': "activepower", 'factor': 100})
 #listChannels.append({'description': "P3", 'register': 51298, 'words': 1, 'unit': 0xFF, 'measurement': "activepower", 'factor': 100})
 listChannels.append({'description': "Q", 'register': 51293, 'words': 1, 'unit': 0xFF, 'measurement': "reactivepower", 'factor': 100})
-listChannels.append({'description': "Q1", 'register': 51299, 'words': 1, 'unit': 0xFF, 'measurement': "reactivepower", 'factor': 100})
+#listChannels.append({'description': "Q1", 'register': 51299, 'words': 1, 'unit': 0xFF, 'measurement': "reactivepower", 'factor': 100})
 #listChannels.append({'description': "Q2", 'register': 51300, 'words': 1, 'unit': 0xFF, 'measurement': "reactivepower", 'factor': 100})
 #listChannels.append({'description': "Q3", 'register': 51301, 'words': 1, 'unit': 0xFF, 'measurement': "reactivepower", 'factor': 100})
 
-strURL = "http://192.168.178.45/vz/htdocs/middleware.php"
-strName = "Molitor"
 # =======================
 # Initialization
 # =======================
 
-print("Used Python version: ")
-print(sys.version)
+# Load data from config.ini
+config = ConfigParser.ConfigParser()
+config.readfp(open(r'config.ini'))
+# main group UUID
+mainGrpUUID = config.get('Status', 'uuid')
+# URL to VZ middleware.php
+strURL = config.get('General', 'url')
+
+_testVZ = testVZ()
+
+if _testVZ == False:
+	print "Something is wrong with VZ server or URL."
+	exit()
+
+print "Version of VZ middleware:", _testVZ
+
+# Reading frequency in seconds
+intervalTime = config.get('General', 'intervalTime')
+# Load name of location or measurement title
+strName = config.get('General', 'name')
+# Load IP of Socomec
+strIP = config.get('General', 'IPsocomec')
+
 
 # Initialize Modbus client
-client = ModbusTcpClient("192.168.178.19")
+client = ModbusTcpClient(strIP)
 ret = client.connect()
 if ret:
-	print "Connected."
+	print "Connected to Socomec."
 else:
-	print "Connection failed."
+	print "Connection to Socomec failed."
 	exit()
 
 # Start the scheduler
 sched = BlockingScheduler()
 
-# Parse config file and check if already a main uuid has been created. If not: create one (initial start of program)
-config = ConfigParser.ConfigParser()
-config.readfp(open(r'config.ini'))
-mainGrpUUID = config.get('General', 'uuid')
-
+# Check if main group already exists. If not (at first start): create one
 if mainGrpUUID == "":
-	print "no uuid"
+	print "No main group exists. Going to create one ..."
 	# create main uuid
 	mainGrpUUID = createGroup(strName, 1)
 	print mainGrpUUID
-	config.set('General', 'uuid', mainGrpUUID)
+	config.set('Status', 'uuid', mainGrpUUID)
 	with open(r'config.ini', 'wb') as configfile:
 		config.write(configfile)
 else:
@@ -169,10 +205,11 @@ for x in listGroups:
 
 for c in listChannels:
 	strMeasurement = c['measurement']
+	_uuid = 0
 
 	# Check if subgroup already exists
 	if strMeasurement not in subGroups:
-		_uuid = createGroup(strMeasurement, 1)
+		_uuid = createGroup(strMeasurement, 0)
 		subGroups[strMeasurement] = _uuid
 		print "Group created:", strMeasurement, addToGroup(mainGrpUUID, _uuid)
 
@@ -180,33 +217,26 @@ for c in listChannels:
 	if (c['measurement'] == "voltage"):
 		# Create channel for measurement
 		_uuid = createChannel("voltage",c['description'])
-		# Add channel to subgroup
-		print "Added to group successully:", addToGroup(subGroups[c['measurement']], _uuid)
-		# Store UUID
-		c['uuid'] = _uuid
 	elif (c['measurement'] == "frequency"):
 		# Create channel for measurement
 		_uuid = createChannel("frequency",c['description'])
-		# Add channel to subgroup
-		print "Added to group successully:", addToGroup(subGroups[c['measurement']], _uuid)
-		# Store UUID
-		c['uuid'] = _uuid
 	elif (c['measurement'] == "activepower") or (c['measurement'] == "reactivepower"):
 		# Create channel for measurement
 		_uuid = createChannel("powersensor",c['description'])
-		# Add channel to subgroup
-		print "Added to group successully:", addToGroup(subGroups[c['measurement']], _uuid)
-		# Store UUID
-		c['uuid'] = _uuid
 	else:
 		print "Measurement type not known."
 		exit()
+
+	# Add channel to subgroup
+	print "Added to group successully:", addToGroup(subGroups[c['measurement']], _uuid)
+	# Store UUID
+	c['uuid'] = _uuid
 
 # =======================
 # Main
 # =======================
 
-sched.add_job(readChannels, 'interval', seconds=frequency)
+sched.add_job(readChannels, 'interval', seconds=int(intervalTime))
 sched.start()
 
 client.close()
