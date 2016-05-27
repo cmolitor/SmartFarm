973f4f8fb8f95b2d316723f2e8b5abf9b54225b7
christoph <christoph.molitor@posteo.de>
2016-05-27 23:17:34 +0200

    added first pyomo scheduling example

---
+++ pyomo/Readme.md
@@ -0,0 +1,3 @@
+This is a demonstration on how to use pyomo for modelling a scheduling problem of a heating system.
+
+The equation of the model are given in the file "Equations.pdf" in this folder.
---
+++ pyomo/scheduling.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# start with following command in terminal: pyomo solve --solver=glpk /Users/christoph/Desktop/scheduling.py
+
+# Import
+import numpy as np
+import platform
+import matplotlib.pyplot as plt
+from pyomo.environ import *
+from pyomo.core import Var
+
+#=====================================================================================
+# Input data
+## Thermal demand in W
+time = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24])
+demandThermal = np.array([4021.85,4054.65,4057.08,4040.75,4019.74,3987.02,3968.28,3969.74,
+                          3965.96,3871.5,3817.4,3773.91,3623.16,3498.71,3488.95,3578.58,
+                          3783.95,3823.33,3870.06,3905.22,3907.65,3895.94,3902.9,3879.62])
+
+price = np.array([0.0558, 0.0548, 0.0499, 0.0479, 0.0486, 0.0522, 0.0655, 0.0920, 
+                  0.1138, 0.1134, 0.1175, 0.1265, 0.1260, 0.1315, 0.1347, 0.1363, 
+                  0.1297, 0.1137, 0.1095, 0.1034, 0.0817, 0.0750, 0.0816, 0.0700])
+
+Qdot_heater = 5000 # Nominal thermal power of heater
+Q_storage_cap = 40000000 # Storage capacity in Ws
+P_heater = -2000 # Nominal electricl power of heater; <0: generator
+TimeStep = 3600 # Length of time intervals; e.g. 1 hour: 3600s
+SoC_min = 0 # Minimum allowed State of Charge (SoC)
+SoC_max = 1 # Maximum allowed State of Charge (SoC)
+SoC_ini = 0.5 # Initial State of Charge (SoC)
+
+#=====================================================================================
+# Methods for parameter and variable initialization
+
+# Init of thermal demand
+def init_thermaldemand(model, i):
+  return float(demandThermal[i-1])
+
+# Init of electricity price
+def init_prices(model, i):
+  return float(price[i-1])
+
+#=====================================================================================
+# Creation of an Abstract Model
+model = AbstractModel()
+
+model.T = Set(initialize=range(1,25)) # values: 1..24
+model.T0 = Set(initialize=range(0,25)) # values: 0..24
+
+# Parameters
+model.qdmnd = Param(model.T, initialize=init_thermaldemand, doc="Thermal demand")
+model.pricegrid = Param(model.T, initialize=init_prices, doc="Electricity price")
+model.qheater = Param(initialize=Qdot_heater, doc="Nominal thermal power of heater in W")
+model.QstrgCap = Param(initialize=Q_storage_cap, doc="Storage capacity in Ws")
+model.pheater = Param(initialize=P_heater, doc="Nominal electrical power of heater in W")
+model.dt = Param(initialize=TimeStep, doc="time constant")
+model.SoCmin = Param(initialize=SoC_min, doc="Minimum SoC of storage")
+model.SoCmax = Param(initialize=SoC_max, doc="Maximum SoC of storage")
+
+# Variables
+model.modlvl = Var(model.T, initialize=0, domain=Binary, doc="Modulation level") # domain=Binary, NonNegativeReals ; bounds=(0,6)
+model.qstrg = Var(model.T, initialize=0, doc="Power flow storage")
+model.Qstrg = Var(model.T0, initialize=SoC_ini*Q_storage_cap, doc="Energy in storage")
+model.pgrid = Var(model.T, initialize=0, doc="Power export to electrical grid")
+
+# Constraints
+def cnstrThermalBalance(model, t):
+  return (-model.qdmnd[t] == -model.modlvl[t] * model.qheater + model.qstrg[t])
+
+def cnstrStorageBalance(model, t):
+  # return the expression for the constraint for i
+  return (model.Qstrg[t] == model.Qstrg[t-1] + model.qstrg[t] * model.dt)
+
+def cnstrSoC(model, t):
+  # return the expression for the constraint for i
+  return (model.SoCmin <= model.Qstrg[t]/model.QstrgCap <= model.SoCmax)
+  
+def cnstrGrid(model, t):
+  # return the expression for the constraint for i
+  return (model.pheater * model.modlvl[t] == model.pgrid[t])
+
+def obj_expression(model):
+  return sum(model.pgrid[t] * model.pricegrid[t] for t in model.T)
+
+model.obj = Objective(rule=obj_expression, sense=minimize)
+
+model.CnstrThermalBalance = Constraint(model.T, rule=cnstrThermalBalance)
+model.CnstrStorageBalance = Constraint(model.T, rule=cnstrStorageBalance)
+model.CnstrSoC = Constraint(model.T, rule=cnstrSoC)
+model.CnstrGrid = Constraint(model.T, rule=cnstrGrid)
+
+#=====================================================================================
+# Postprocessing
+
+# Read values of model variables and return list with values
+def readVarValues(var_name, instance):
+  listReturn = []
+  for v in instance.component_objects(Var, active=True):
+    #print("Variable", v)
+    if str(v) == str(var_name):
+      varobject = getattr(instance, str(v))
+      for index in varobject:
+        #print(varobject[index].value)
+        listReturn.append(varobject[index].value)
+  return listReturn
+
+def pyomo_postprocess(options=None, instance=None, results=None):
+  print(platform.python_version())
+
+  modlvl = readVarValues("modlvl", instance)
+  Qstrg = readVarValues("Qstrg", instance)
+  PGrid = readVarValues("pgrid", instance)
+
+  width = 4 # in inches
+  height = 4
+  # Plot the data
+  fig, ax = plt.subplots(dpi=500, nrows=4, ncols=1, sharex=True, sharey=False) # figsize=(width, height)
+  plot1 = ax[0].step(time, price, color='#0072B2')
+  plot2 = ax[1].step(time, modlvl, color='#0072B2')
+  plot3 = ax[2].step(time, Qstrg[1:], color='#0072B2')
+  plot4 = ax[3].step(time, PGrid, color='#0072B2')
+
+  ax[0].set_xlim(time[0], time[23])
+  ax[1].set_ylim(-0.1, 1.1)
+  ax[3].set_ylim(min(PGrid)*1.1, max(PGrid)*1.1)
+
+  ax[3].set_xlabel("Time in hours")
+  ax[0].set_ylabel("Price in \n €/MWh")
+  ax[1].set_ylabel("Modulation \n level")
+  ax[2].set_ylabel("Stored energy \n in Ws")
+  ax[3].set_ylabel("P Grid \n in W")
+
+  fig.subplots_adjust(left=0.14, right=0.98, top=0.98, hspace=0.05, wspace=0.02)
+
+  fig.savefig("schedule.pdf")
+
+  #plt.show()
+
+
+
+  # for v in instance.component_objects(Var, active=True):
+  #   #print("Variable", v)
+  #   if str(v) == "modlvl":
+  #     varobject = getattr(instance, str(v))
+  #     for index in varobject:
+  #       print(varobject[index].value)
+
+#=====================================================================================
+# This is an optional code path that allows the script to be run outside of
+# pyomo command-line.  For example:  python transport.py
+if __name__ == '__main__':
+  print("Python version: ", platform.python_version())
+  # This emulates what the pyomo command-line tools does
+  from pyomo.opt import SolverFactory
+  import pyomo.environ
+  opt = SolverFactory("glpk")
+  instance = model.create()
+  results = opt.solve(instance)
+  #sends results to stdout
+  results.write()
+  print("\nDisplaying Solution\n" + '-'*60)
+  pyomo_postprocess(None, instance, results)
