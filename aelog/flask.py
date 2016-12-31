c420ddee933add6f5b735599d1454d9785cfe2c8
christoph <christoph.molitor@posteo.de>
2016-12-31 09:30:32 +0100

    flask version

--- aelog/flask.py
+++ aelog/flask.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 @app.route("/sinvertwebmonitor/InverterService/InverterService.asmx/CollectInverterData", methods=['POST'])
 def dataCollector():
-    data = request.form['xmlData']
-    print(data)
-    # parseData. Take a look at ElementTree
+	data = request.form['xmlData']
+	print(data)
+	# parseData. Take a look at ElementTree
 
 if __name__ == "__main__":
-    app.run(host=0.0.0.0, port=80)
+	app.run(host=0.0.0.0, port=80)
